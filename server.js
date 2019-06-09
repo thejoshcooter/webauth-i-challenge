93da943901523ee996fea644482adf49d44debfd
Josh Cooter <thejoshcooter@gmail.com>
2019-06-09 18:52:09 -0500

    setup basic endpoint functionality

--- server.js
+++ server.js
@@ -15,17 +15,46 @@ const db = knex(knexConfig.development);
 
 // register user endpoint
 server.post('/api/register', (req, res) => {
-    res.status(200).json({ message: 'success' })
+    // res.status(200).json({ message: 'success' })
+    db('users')
+    .insert(req.body)
+    .then(user => {
+        res.status(201).json(user)
+    })
+    .catch(err => {
+        res.status(400).json({ error: 'error registering user', err })
+    })
 });
 
 // user login endpoint
 server.post('/api/login', (req, res) => {
-    res.status(200).json({ message: 'success' })
+    // res.status(200).json({ message: 'success' })
+    const { username, password } = req.body;
+    db('users')
+    .where({ username })
+    .first()
+    .then(user => {
+        if(user) {
+            res.status(200).json(user)
+        } else {
+            res.status(400).json({ error: 'user does not exist' })
+        }
+    })
+    .catch(err => {
+        res.status(404).json({ error: 'user not found', err })
+    })
 });
 
 // get users endpoint
 server.get('/api/users', (req, res) => {
-    res.status(200).json({ message: 'success' });
+    // res.status(200).json({ message: 'success' });
+    db('users')
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(err => {
+        res.status(404).json({ error: 'user not found', err })
+    })
 });
 
 // default fallback
