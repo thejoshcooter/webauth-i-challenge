44c0b12819631593a1f142bd0bea2d4afeeb8f0a
Josh Cooter <thejoshcooter@gmail.com>
2019-06-09 17:33:47 -0500

    setup knex and stub out endpoints

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 // import express
 const express = require('express');
+// import knex 
+const knex = require('knex');
+
 // implement server
 const server = express();
 // body parser
 server.use(express.json());
+
+// import knex config
+const knexConfig = require('./knexfile');
+// implement knex db connection
+const db = knex(knexConfig.development);
+
+// register user endpoint
+server.post('/api/register', (req, res) => {
+    res.status(200).json({ message: 'success' })
+});
+
+// user login endpoint
+server.post('/api/login', (req, res) => {
+    res.status(200).json({ message: 'success' })
+});
+
+// get users endpoint
+server.get('/api/users', (req, res) => {
+    res.status(200).json({ message: 'success' });
+});
 
 // default fallback
 server.get('/', (req, res) => {
