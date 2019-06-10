6286af6f94324ec145b92264574afeb6086a7e29
Josh Cooter <thejoshcooter@gmail.com>
2019-06-10 01:55:09 -0500

    mvp complete except for sessions and cookies stretch.

--- server.js
+++ server.js
@@ -13,11 +13,18 @@ const knexConfig = require('./knexfile');
 // implement knex db connection
 const db = knex(knexConfig.development);
 
+// import bcrypt
+const bcrypt = require('bcrypt');
+
 // register user endpoint
 server.post('/api/register', (req, res) => {
     // res.status(200).json({ message: 'success' })
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 12)
+    user.password = hash;
+
     db('users')
-    .insert(req.body)
+    .insert(user)
     .then(user => {
         res.status(201).json(user)
     })
@@ -34,14 +41,14 @@ server.post('/api/login', (req, res) => {
     .where({ username })
     .first()
     .then(user => {
-        if(user) {
+        if(user && bcrypt.compareSync(password, user.password)) {
             res.status(200).json(user)
         } else {
-            res.status(400).json({ error: 'user does not exist' })
+            res.status(400).json({ error: 'you shall not pass' })
         }
     })
     .catch(err => {
-        res.status(404).json({ error: 'user not found', err })
+        res.status(404).json({ error: 'error', err })
     })
 });
 
